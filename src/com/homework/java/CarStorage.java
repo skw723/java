7885bf97dabba05d7c6443a1c3cd8b34a44d4514
nt60052 <nt60052@nhn.com>
2015-04-15 16:43:00 +0900

    [심규원][NOBTS] CarStorage 변경(array -> ArrayList)
    Change-Id: I062fc8fb7ad10f13c85dd3c950b4960488d04b0c

--- src/com/homework/java/CarStorage.java
+++ src/com/homework/java/CarStorage.java
@@ -1,62 +1,53 @@
 package com.homework.java;
 
+import java.util.ArrayList;
+
 /**
+ * v0.1
  * Car 객체의 저장을 담당하는 클래스
  * array로 구현되어있음
  * 기본 저장공간 : 100
  * 저장공간이 부족하면 extend()를 통하여 저장공간 2배 확장
  * 객체 삭제 시 시프트 이동을 통하여 빈공간 제거
+ * 
+ * v0.2
+ * array -> ArrayList 로 변경
+ * 저장공간 확장을 하지 않음
+ * 사용되지 않는 메소드 제거(isFull, extend)
+ * 삭제 시 시프트 이동 없음
  * @author 심규원
  *
  */
 public class CarStorage {
-	private Car[] cars;
-	private int maxCars;
-	private int carCount;
+	private ArrayList<Car> carList;
 	
 	public CarStorage(){
-		this.maxCars = 100;
-		cars = new Car[maxCars];
-		this.carCount = 0;
+		carList = new ArrayList<Car>();
 	}
 	public boolean addCar(Car car){
-		if(isFull()){
-			extend();
-		}
-		cars[carCount++] = car;
-		return true;
+		return carList.add(car);
 	}
 	/**
+	 * v0.1
 	 * 저장된 Car객체를 삭제하는 메소드
 	 * 삭제 성공 시 시프트를 통하여 빈공간 제거
+	 * 삭제 실패 시 null 리턴
+	 * 
+	 * v0.2
+	 * ArrayList로 변경에 따라 삭제 후 시프트이동이 없음
 	 * @param carId - 삭제할 객체의 carId
 	 * @return - 삭제 성공여부
 	 */
 	public Car deleteCar(int carId){
 		int index = indexOf(carId);
 		if(index == -1){
-			return null;			
+			return null;
 		}
-		Car result = cars[index];
-		cars[index] = null;
-		System.arraycopy(cars, index+1, cars, index, carCount - index);
-		carCount--;
-		return result;
-	}
-	public boolean isFull(){
-		if(carCount == maxCars){
-			return true;
+		else{
+			Car result = carList.get(index);
+			carList.remove(index);
+			return result;
 		}
-		return false;
-	}
-	/**
-	 * 저장공간을 현재 크기으 ㅣ2배로 늘리는 메소드
-	 */
-	private void extend(){
-		maxCars *= 2;
-		Car[] newCars = new Car[maxCars];
-		System.arraycopy(cars, 0, newCars, 0, carCount);
-		cars = newCars;
 	}
 	/**
 	 * 해당 index의 Car 객체를 리턴하는 메소드
@@ -64,13 +55,13 @@ public class CarStorage {
 	 * @return - 해당 Car 객체
 	 */
 	public Car getCar(int index){
-		return cars[index];
+		return carList.get(index);
 	}
 	/**현재 저장된 Car 객체의 개수를 리턴
 	 * @return - 저장된 Car객체의 수
 	 */
 	public int getCarCount(){
-		return carCount;
+		return carList.size();
 	}
 	/**
 	 * carId를 가진 객체의 index를 검색하는 메소드
@@ -78,7 +69,7 @@ public class CarStorage {
 	 * @return - 해당 Car객체의 index(return이 -1인 경우 검색 실패)
 	 */
 	public int indexOf(int carId){
-		for(int i=0; i<carCount; i++){
+		for(int i=0; i<carList.size(); i++){
 			if(getCar(i).getCarInfo().getId() == carId){
 				return i;
 			}
